3f79eabdd6734b822df180c508bf57a87bee35cb
Fernando Zavalia <zavaliafernando@gmail.com>
2019-01-22 01:27:19 -0300

    About page layout

--- pages/about.js
+++ pages/about.js
@@ -11,22 +11,57 @@ const AboutPage = ({ pathname }) => {
         style={{
           display: "flex",
           alignItems: "center",
-          flexDirection: "column",
-          marginTop: 80
+          flexDirection: "column"
         }}
       >
         <img
-          style={{ height: 250, boxShadow: "0 0 5px 0 dimgrey" }}
+          style={{
+            height: 250,
+            boxShadow: "0 0 10px 0 #313131",
+            margin: "80px 0 40px 0"
+          }}
           src={"/static/img/portrait.jpeg"}
         />
         <span
           style={{
-            marginTop: 80,
             fontSize: 20,
-            color: constants.colors.text.default
+            color: constants.colors.text.default,
+            marginBottom: 20
           }}
         >
           Fernando Alberto Zavalia Paunero
+        </span>
+        <span
+          style={{
+            fontSize: 14,
+            color: constants.colors.text.default,
+            marginBottom: 40
+          }}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In fermentum,
+          massa at iaculis posuere
+        </span>
+        <span
+          style={{
+            fontSize: 14,
+            color: constants.colors.text.default,
+            width: "80%"
+          }}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In fermentum,
+          massa at iaculis posuere, nunc quam rutrum libero, vulputate tristique
+          leo odio sed lorem. Ut sit amet nisi et lorem accumsan sagittis sed at
+          nulla. Integer malesuada lacinia est non rhoncus. Duis ut ultrices
+          magna. Integer eget ultrices tellus. Morbi porta facilisis quam, vitae
+          vestibulum mauris dapibus sed. Curabitur suscipit orci vel quam
+          iaculis convallis. Suspendisse erat turpis, ultricies et est sit amet,
+          rutrum elementum nunc. Etiam pellentesque facilisis massa, id
+          consequat ante ornare et. Integer tempor scelerisque ante, et ultrices
+          orci maximus ac. Suspendisse sit amet tincidunt augue. Proin venenatis
+          lobortis ultrices. Nulla risus orci, bibendum sagittis porttitor ut,
+          sodales ac neque. In ac arcu ac metus tincidunt convallis nec sit amet
+          diam. Aenean efficitur efficitur venenatis. Sed in euismod velit. Duis
+          bibendum tortor turpis, elementum rutrum nunc facilisis et.
         </span>
       </div>
     </Layout>
