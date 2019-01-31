00cf72825842cfb91f8d3069c34c2c3824fd588e
Fernando Zavalia <zavaliafernando@gmail.com>
2019-01-30 23:53:50 -0300

    Initial localizaton commit

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const next = require("next");
+const fs = require("fs");
+const fsp = fs.promises;
+const path = require("path");
 
 const nextApp = next({ dev: process.env.NODE_ENV !== "production" });
 const nextHandler = nextApp.getRequestHandler();
 
 nextApp.prepare().then(() => {
-  
+
   const server = express();
+
+  server.get("/locale/:lang", getLocaleHandler);
 
   server.get("/", (_, res) => res.redirect("/about"));
 
@@ -15,7 +20,27 @@ nextApp.prepare().then(() => {
   const port = process.env.PORT || 3000;
 
   server.listen(port, err => {
+
     if (err) throw err;
+    
     console.log(`> Ready on http://localhost:${port}`);
   });
 });
+
+const getLocaleHandler = (req, res) => {
+
+  const lang = req.params["lang"]
+
+  let localePath = makeLangPath(lang);
+
+  fs.exists(localePath, async exists => {
+    
+    if (!exists) localePath = makeLangPath('en')
+
+    const data = await fsp.readFile(localePath, { encoding: "utf8" });
+    
+    res.send(data);
+  });
+};
+
+const makeLangPath = lang => path.join(__dirname, "static", "locale", lang + ".json")
