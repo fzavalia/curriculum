e3b4bdcaaf04ecd195ff4478c6e46225fe1c8bea
Fernando Zavalia <zavaliafernando@gmail.com>
2019-02-02 03:04:23 -0300

    Move GA track id to const

--- utils/ga.js
+++ utils/ga.js
@@ -1,10 +1,12 @@
 import { Component } from "react";
 
+const trackID = "UA-133674055-1";
+
 export const GAHeadElement = () => (
   <div>
     <script
       async
-      src="https://www.googletagmanager.com/gtag/js?id=UA-133674055-1"
+      src={"https://www.googletagmanager.com/gtag/js?id=" + trackID}
     />
     <script
       dangerouslySetInnerHTML={{
@@ -12,7 +14,7 @@ export const GAHeadElement = () => (
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'UA-133674055-1', { 'send_page_view': false });
+gtag('config', '${trackID}', { 'send_page_view': false });
 `
       }}
     />
@@ -21,8 +23,8 @@ gtag('config', 'UA-133674055-1', { 'send_page_view': false });
 
 export class PageView extends Component {
   componentDidMount = () => {
-    window.gtag("config", "UA-133674055-1", { page_path: this.props.pathname });
+    console.log(process.env)
+    window.gtag("config", trackID, { page_path: this.props.pathname });
   };
-
   render = () => <div />;
 }
